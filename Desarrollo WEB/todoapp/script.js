97043d541cf467aaa78d12f60ab5f86e21cd743d
Angel Francisco Sique Santos <asique@minex.gob.gt>
2023-10-17 14:02:08 -0600

    Primera versión de todo

---
+++ Desarrollo WEB/todoapp/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const tareaInput = document.getElementById('tareaInput');
+    const agregarButton = document.getElementById('agregar');
+    const listaTareas = document.getElementById('listaTareas');
+
+    agregarButton.addEventListener('click', function () {
+        const tareaTexto = tareaInput.value.trim();
+
+        if (tareaTexto === '') {
+            alert('Por favor, ingrese una tarea válida.');
+            return;
+        }
+
+        const nuevaTarea = document.createElement('div');
+        nuevaTarea.classList.add('task');
+        nuevaTarea.textContent = tareaTexto;
+
+        nuevaTarea.addEventListener('click', function () {
+            nuevaTarea.classList.toggle('completed');
+        });
+
+        nuevaTarea.addEventListener('contextmenu', function (event) {
+            event.preventDefault();
+            nuevaTarea.remove();
+        });
+
+        listaTareas.appendChild(nuevaTarea);
+        tareaInput.value = '';
+    });
+});
